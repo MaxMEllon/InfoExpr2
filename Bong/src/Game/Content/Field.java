5424aa1d9157e0341fff459f08da0c41c8288b07
mille-f <f11235tetra@gmail.com>
2015-11-11 21:13:06 +0900

    [add] Background

--- Bong/src/Game/Content/Field.java
+++ Bong/src/Game/Content/Field.java
@@ -1,7 +1,9 @@
 package Game.Content;
 
+import java.awt.Color;
 import java.awt.Graphics;
 import java.util.ArrayList;
+
 import Common.BongPanel;
 import Common.Size;
 import Game.Bong;
@@ -47,5 +49,9 @@ public class Field extends BongPanel
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
+        g.setColor(Color.black);
+        g.fillRect(0, 0, size.Width(), size.Height());
+        g.setColor(Color.green);
+        g.drawLine((int)size.Width()/2, 0, (int)size.Width()/2, size.Height());
     }
 }
