89b23053d36333ab4f80e5968372831d0319ea63
mille-f <f11235tetra@gmail.com>
2015-11-22 17:22:21 +0900

    [add] 当たり判定メソッドの仮実装

--- Bong/src/Common/BongPanel.java
+++ Bong/src/Common/BongPanel.java
@@ -1,7 +1,12 @@
 package Common;
 
 import javax.swing.JPanel;
+
+import Game.Content.Ball.Ball;
+
 import java.awt.Dimension;
+import java.awt.Rectangle;
+import java.awt.geom.Area;
 
 public class BongPanel extends JPanel
 {
@@ -39,8 +44,10 @@ public class BongPanel extends JPanel
         return this.size.Height();
     }
 
-    public boolean isHit(Area area) {
-        return false;
+    public boolean isHit(Ball b) {
+        Rectangle objRec = new Rectangle(getX(), getY(), getWidth(), getHeight());
+        Rectangle ballRec = new Rectangle(b.getX(), b.getY(), b.getWidth(), b.getHeight());
+        return ballRec.intersects(objRec);
     }
     
     public Area getArea() {
