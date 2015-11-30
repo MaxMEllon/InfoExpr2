7dff89fe939a72fb164e986873d413f15365eb81
joniyjoniy <s13t264@stmail.eng.kagawa-u.ac.jp>
2015-11-30 16:35:27 +0900

    [modify] Logo read

--- Bong/src/Game/Title.java
+++ Bong/src/Game/Title.java
@@ -8,31 +8,29 @@ import java.net.URI;
 import java.net.URL;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
 import javax.imageio.ImageIO;
 import javax.swing.JPanel;
+
 import java.awt.Color;
 import java.awt.Label;
 import java.awt.Font;
 import java.applet.*;
+
 import javax.imageio.ImageIO;
 import javax.swing.*;
 
 import Common.BongPanel;
+import Game.Content.Logo;
 
 public class Title extends BongPanel
 {
     private static final long serialVersionUID = 6838266341443127470L;
-    private BufferedImage image;
+    private Logo image;
 
     public Title(URL base) {
         this.setLayout(null);
-        try {
-            System.out.println(base);
-            URL url = new URL(base + "../Bong/assets/images/logo.png");
-            image = ImageIO.read(url);
-        } catch (IOException ex) {
-            System.out.println("cant read image file");
-        }
+        image = new Logo();
         this.setBounds(0, 0, 800, 400);
     }
 
